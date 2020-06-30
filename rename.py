ca3e842335ed37f69cb9c3687adeabc39be597e4
TheNetAdmin <zxwang42@gmail.com>
2020-06-30 00:53:09 -0700

    Initial commit

---
+++ rename.py
@@ -0,0 +1,44 @@
+from pathlib import Path
+import os
+import click
+import re
+
+@click.command()
+@click.argument("path")
+def anima_rename(path):
+    os.chdir(path)
+    path = Path(".")
+    rename_suffix = [".mkv", ".mka", ".acc"]
+    src_fname = []
+    for f in path.iterdir():
+        if not f.is_dir() and f.suffix in rename_suffix:
+            src_fname.append(f.name)
+    
+    common_entries = [
+        r"(\s)*\[VCB-Studio\](\s)*",
+        r"(\s)*\[Ma10p_1080p\](\s)*",
+        r"(\s)*\[x265_flac\](\s)*"
+    ]
+
+    dst_fname = []
+    for f in src_fname:
+        for entry in common_entries:
+            p = re.compile(entry)
+            f = p.sub('', f)
+        # remove final brackets
+        f = f.replace('[', '')
+        f = f.replace(']', '')
+        dst_fname.append(f)
+
+    with open('rename.log', 'w') as logfile:
+        for i in range(len(src_fname)):
+            print(f'|{src_fname[i]}| --> |{dst_fname[i]}|')
+            logfile.write(f'|{src_fname[i]}| --> |{dst_fname[i]}|\n')
+
+    for i in range(len(src_fname)):
+        sf = Path(src_fname[i])
+        sf.rename(dst_fname[i])
+
+
+if __name__ == "__main__":
+    anima_rename()
